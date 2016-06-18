460489f094a1db3ca20302c3230a2358cf3a6342
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-06-19 01:01:51 +0800

    设置存储文件夹

--- Hentai Viewer/Settings.cs
+++ Hentai Viewer/Settings.cs
@@ -1,16 +1,49 @@
-﻿namespace Meowtrix.HentaiViewer
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+
+namespace Meowtrix.HentaiViewer
 {
     class Settings : NotificationObject
     {
         private Settings() { }
         public static Settings Current { get; } = new Settings();
-        public void Load()
+        public void Load() => LoadAsync().Wait();
+        public async Task LoadAsync()
         {
-
+            if (StorageApplicationPermissions.FutureAccessList.ContainsItem("StorageFolder"))
+                _folder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("StorageFolder");
+            else
+            {
+                _folder = KnownFolders.SavedPictures;
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("StorageFolder", _folder);
+            }
         }
         public void Save()
         {
 
+        }
+
+        private StorageFolder _folder;
+        public string StorageFolder => _folder.Path;
+
+        public async void ChangeStorageFolder()
+        {
+            var picker = new FolderPicker();
+            picker.ViewMode = PickerViewMode.List;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+            picker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+            StorageFolder folder = await picker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("StorageFolder", folder);
+                _folder = folder;
+                OnPropertyChanged(nameof(StorageFolder));
+            }
         }
     }
 }
