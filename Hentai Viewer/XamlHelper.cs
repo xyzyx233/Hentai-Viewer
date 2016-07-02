586aad1aefc7a314f104be5554c206dbf12104ab
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-02 15:02:59 +0800

    使用XamlHelper代替Converter

---
+++ Hentai Viewer/XamlHelper.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml;
+
+namespace Meowtrix.HentaiViewer
+{
+    internal static class XamlHelper
+    {
+        public static bool GetIsVisible(UIElement obj) => obj.Visibility == Visibility.Visible;
+        public static void SetIsVisible(UIElement obj, bool value) => obj.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        public static bool GetIsNotVisible(UIElement obj) => obj.Visibility != Visibility.Visible;
+        public static void SetIsNotVisible(UIElement obj, bool value) => obj.Visibility = value ? Visibility.Collapsed : Visibility.Visible;
+    }
+}
