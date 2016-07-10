08995abb0ae959f9ea9936cd6a2e43345f475b92
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-10 19:46:36 +0800

    加载占位符

---
+++ Hentai Viewer/ViewModels/PlaceHolderPage.cs
@@ -0,0 +1,9 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Meowtrix.HentaiViewer.ViewModels
+{
+    class PlaceHolderPage : ViewPage
+    {
+        public override Symbol HeaderIcon => Symbol.More;
+    }
+}
--- Hentai Viewer/ViewModels/ViewPageSelector.cs
+++ Hentai Viewer/ViewModels/ViewPageSelector.cs
@@ -8,9 +8,11 @@ namespace Meowtrix.HentaiViewer.ViewModels
     {
         public DataTemplate ListTemplate { get; set; }
         public DataTemplate DetailTemplate { get; set; }
+        public DataTemplate PlaceHolderTamplate { get; set; }
         protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
         {
-            if (item is ListPage) return ListTemplate;
+            if (item is PlaceHolderPage) return PlaceHolderTamplate;
+            else if (item is ListPage) return ListTemplate;
             else if (item is DetailPage) return DetailTemplate;
             else throw new ArgumentException(nameof(item));
         }
