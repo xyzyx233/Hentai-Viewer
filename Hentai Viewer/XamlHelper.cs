68ddf1cab711461483dc0c163c268bf6dd7740cc
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-10 13:57:51 +0800

    附加属性扩展方法化

--- Hentai Viewer/XamlHelper.cs
+++ Hentai Viewer/XamlHelper.cs
@@ -4,9 +4,9 @@ namespace Meowtrix.HentaiViewer
 {
     internal static class XamlHelper
     {
-        public static bool GetIsVisible(UIElement obj) => obj.Visibility == Visibility.Visible;
-        public static void SetIsVisible(UIElement obj, bool value) => obj.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
-        public static bool GetIsNotVisible(UIElement obj) => obj.Visibility != Visibility.Visible;
-        public static void SetIsNotVisible(UIElement obj, bool value) => obj.Visibility = value ? Visibility.Collapsed : Visibility.Visible;
+        public static bool GetIsVisible(this UIElement obj) => obj.Visibility == Visibility.Visible;
+        public static void SetIsVisible(this UIElement obj, bool value) => obj.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        public static bool GetIsNotVisible(this UIElement obj) => obj.Visibility != Visibility.Visible;
+        public static void SetIsNotVisible(this UIElement obj, bool value) => obj.Visibility = value ? Visibility.Collapsed : Visibility.Visible;
     }
 }
