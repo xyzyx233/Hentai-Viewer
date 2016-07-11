a9c32164dddbda26761d65325c3920c38a94203a
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-11 20:11:59 +0800

    共享HttpHost

---
+++ Hentai Viewer/HttpHost.cs
@@ -0,0 +1,13 @@
+﻿using Windows.Web.Http;
+
+namespace Meowtrix.HentaiViewer
+{
+    internal static class HttpHost
+    {
+        public static HttpClient Client { get; } = new HttpClient();
+        static HttpHost()
+        {
+            Client.DefaultRequestHeaders.UserAgent.TryParseAdd(Settings.UAString);
+        }
+    }
+}
