03dbea8c3f5030444f30f189e2f3a804c4a217cb
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-11 23:46:50 +0800

    ehentai:uconfig

--- Hentai Viewer/Providers/EHentaiProvider.cs
+++ Hentai Viewer/Providers/EHentaiProvider.cs
@@ -9,12 +9,19 @@ using Windows.ApplicationModel.Resources;
 using Windows.Storage;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
+using Windows.Web.Http;
 
 namespace Meowtrix.HentaiViewer.Providers
 {
     [Export(typeof(Composition.IGallery))]
     class EHentaiProvider : Composition.IGallery
     {
+        public EHentaiProvider()
+        {
+            HttpHost.CookieManager.SetCookie(new HttpCookie(nameof(uconfig), "e-hentai.org", "") { Value = uconfig });
+            HttpHost.CookieManager.SetCookie(new HttpCookie(nameof(uconfig), "exhentai.org", "") { Value = uconfig });
+        }
+        private const string uconfig = "uh_y-lt_m-tl_r-tr_2-ts_m-prn_y-dm_t-ar_0-xns_0-rc_0-rx_0-ry_0-cs_a-fs_p-to_a-pn_0-sc_0-ru_rrggb-xr_a-cats_0-ms_n-mt_n-sa_y-oi_n-qb_n-tf_n-hp_-hk_-xl_";
         public string Name => "EHentai";
         public UIElement SettingPage => new EHentaiSetting { Settings = SettingInstance };
         public static EHentaiSettings SettingInstance { get; } = new EHentaiSettings();
