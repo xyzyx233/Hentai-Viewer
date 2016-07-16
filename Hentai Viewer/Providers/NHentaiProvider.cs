d2ed979b2747dbb47514af314cf21bfa70f782d7
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-17 00:44:55 +0800

    NHentaiProvider

--- Hentai Viewer/Providers/NHentaiProvider.cs
+++ Hentai Viewer/Providers/NHentaiProvider.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Composition;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Meowtrix.HentaiViewer.ViewModels;
 using Windows.Storage;
 using Windows.UI.Xaml;
@@ -10,6 +13,7 @@ namespace Meowtrix.HentaiViewer.Providers
     [Export(typeof(Composition.IGallery))]
     class NHentaiProvider : Composition.IGallery
     {
+        private const string hostname = "https://nhentai.net/";
         public string Name => "NHentai";
         public UIElement SettingPage => new NHentaiSettingPage();
         public void LoadSettings(ApplicationDataContainer localdata, ApplicationDataContainer roamingdata)
@@ -20,9 +24,39 @@ namespace Meowtrix.HentaiViewer.Providers
         {
             //throw new NotImplementedException();
         }
-        public Task<SearchResult> SearchAsync(SearchInfo info, int page)
+        private static Uri hosturi = new Uri(hostname);
+        public async Task<SearchResult> SearchAsync(SearchInfo info, int page)
         {
-            throw new NotImplementedException();
+            HtmlDocument document = new HtmlDocument();
+            var sb = new StringBuilder(hostname);
+            sb.Append($"?page={page}");
+            //TODO:add query
+            try
+            {
+                document.LoadHtml(await HttpHost.Client.GetStringAsync(new Uri(sb.ToString())));
+                return new SearchResult
+                {
+                    Provider = this,
+                    SearchInfo = info,
+                    PagesCount = int.Parse(document.DocumentNode.SelectSingleNode("//a[@class='last']").Attributes["href"].Value.Replace("?page=", "")),
+                    Entries = document.DocumentNode.SelectNodes("//div[@class='gallery']/a")
+                        .Select(node => new GalleryEntryInfo
+                        {
+                            Title = node.SelectSingleNode("div").InnerText,
+                            Uri = new Uri(hosturi, node.Attributes["href"].Value),
+                            ThumbnailUri = new Uri("https:" + node.SelectSingleNode("img").Attributes["src"].Value)
+                        }).ToArray()
+                };
+            }
+            catch
+            {
+                return new SearchResult
+                {
+                    Provider = this,
+                    SearchInfo = info,
+                    Entries = new GalleryEntryInfo[0]
+                };
+            }
         }
     }
 }
