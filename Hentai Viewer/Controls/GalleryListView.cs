0861693a128550aa6224b28ae8c390524a1d04e4
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-16 21:09:44 +0800

    GalleryListView:反向增量加载

---
+++ Hentai Viewer/Controls/GalleryListView.cs
@@ -0,0 +1,41 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Meowtrix.HentaiViewer.Controls
+{
+    public class GalleryListView : ListView
+    {
+        private ScrollViewer _scrollViewer;
+
+        public GalleryListView()
+        {
+            this.Loaded += ListViewEx_Loaded;
+            this.Unloaded += ListViewEx_Unloaded;
+        }
+
+        private void ListViewEx_Unloaded(object sender, RoutedEventArgs e)
+        {
+            this.Unloaded -= ListViewEx_Unloaded;
+            if (_scrollViewer != null)
+                _scrollViewer.ViewChanged -= Sv_ViewChanged;
+        }
+
+        private void ListViewEx_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Loaded -= ListViewEx_Loaded;
+            _scrollViewer = this.FindFirstChild<ScrollViewer>();
+            if (_scrollViewer != null)
+                _scrollViewer.ViewChanged += Sv_ViewChanged;
+        }
+
+        private async void Sv_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            if (e.IsIntermediate == false && _scrollViewer.VerticalOffset < 1)
+            {
+                var source = ItemsSource as ISupportReversalLoading;
+                if (source?.HasMoreItems == true)
+                    await source.LoadMoreItemsAsync(1);
+            }
+        }
+    }
+}
