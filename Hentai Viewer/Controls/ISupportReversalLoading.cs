c03712c24df966dd678f019159ae90d85b770170
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-16 20:49:21 +0800

    ISupportReversalLoading

---
+++ Hentai Viewer/Controls/ISupportReversalLoading.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Meowtrix.HentaiViewer.Controls
+{
+    public interface ISupportReversalLoading
+    {
+        bool HasMoreItems { get; }
+        Task<LoadMoreItemsResult> LoadMoreItemsAsync(uint count);
+    }
+}
--- Hentai Viewer/ViewModels/GalleryEntryCollection.cs
+++ Hentai Viewer/ViewModels/GalleryEntryCollection.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using Meowtrix.HentaiViewer.Controls;
 using Windows.Foundation;
 using Windows.UI.Xaml.Data;
 
 namespace Meowtrix.HentaiViewer.ViewModels
 {
-    internal class GalleryEntryCollection : ObservableCollection<GalleryEntryInfo>, ISupportIncrementalLoading
+    internal class GalleryEntryCollection : ObservableCollection<GalleryEntryInfo>, ISupportIncrementalLoading, ISupportReversalLoading
     {
         public ListPage Page { get; }
         public GalleryEntryCollection(ListPage page, IEnumerable<GalleryEntryInfo> entries) : base(entries)
         {
             Page = page;
         }
-        public bool HasMoreItems => Page.topage < Page.TotalPages;
+        bool ISupportIncrementalLoading.HasMoreItems => Page.topage < Page.TotalPages;
+        bool ISupportReversalLoading.HasMoreItems => Page.frompage > 1;
         public void AddRange(IEnumerable<GalleryEntryInfo> items)
         {
             foreach (var item in items) Add(item);
@@ -36,6 +38,7 @@ namespace Meowtrix.HentaiViewer.ViewModels
             if (Page.itemsPerPage < searchresult.Entries.Count) Page.itemsPerPage = searchresult.Entries.Count;
             return (uint)searchresult.Entries.Count;
         }
+        async Task<LoadMoreItemsResult> ISupportReversalLoading.LoadMoreItemsAsync(uint count) => new LoadMoreItemsResult { Count = await LoadPreviousPageAsync() };
         public async Task<uint> LoadPreviousPageAsync()
         {
             var searchresult = await Page.Provider.SearchAsync(Page.SearchInfo, Page.frompage - 1);
